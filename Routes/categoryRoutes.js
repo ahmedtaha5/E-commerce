1b6011cdbb3fd92e3b26d0da9781b14e5d4ecc92
ahmedtaha5 <ahmedtaha1682@gmail.com>
2024-12-02 08:37:36 +0200

    category routes are updated

--- Routes/categoryRoutes.js
+++ Routes/categoryRoutes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const {
   getAllCategories,
+  getCategory,
   createCategory,
-  getCategory
+  updateCategory,
+  deleteCategory
 } = require('../Controllers/categoryControllers');
 
 router
@@ -13,6 +15,10 @@ router
   .get(getAllCategories)
   .post(createCategory);
 
-router.route('/:id').get(getCategory);
+router
+  .route('/:id')
+  .get(getCategory)
+  .patch(updateCategory)
+  .delete(deleteCategory);
 
 module.exports = router;
