307046cf815b2c5b2c07f42664b25ebe35c4dbfd
ahmedtaha5 <ahmedtaha1682@gmail.com>
2024-12-23 02:11:25 +0200

    brand controllers is added

---
+++ Controllers/brandControllers.js
@@ -0,0 +1,30 @@
+const sharp = require('sharp');
+const asyncHandler = require('express-async-handler');
+const { v4: uuidv4 } = require('uuid');
+const Brand = require('../Models/brandModel');
+const handlerFactory = require('../Controllers/handlerFactory');
+const { uploadSingleImage } = require('../Middlewares/uploadImageMiddleware');
+
+exports.resizeImage = asyncHandler(async (req, res, next) => {
+  const filename = `Brand-${uuidv4()}-${Date.now()}.jpeg`;
+  await sharp(req.file.buffer)
+    .resize(320, 240)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`uploads/brands/${filename}`);
+
+  req.body.image = filename;
+  next();
+});
+
+exports.uploadBrandImage = uploadSingleImage('image');
+
+exports.getAllBrands = handlerFactory.getAll(Brand);
+
+exports.getBrand = handlerFactory.getOne(Brand);
+
+exports.createBrand = handlerFactory.createOne(Brand);
+
+exports.updateBrand = handlerFactory.updateOne(Brand);
+
+exports.deleteBrand = handlerFactory.deleteOne(Brand);
