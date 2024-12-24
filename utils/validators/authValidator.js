07349e5d0435d013ae51f783f10383076fa39f41
ahmedtaha5 <ahmedtaha1682@gmail.com>
2024-12-25 01:36:12 +0200

    Auth validators are added

---
+++ utils/validators/authValidator.js
@@ -0,0 +1,43 @@
+const slugify = require('slugify');
+const { check, body } = require('express-validator');
+const validatorMiddleware = require('../../Middlewares/validatorMiddleware');
+const User = require('../../Models/userModel');
+
+exports.signupValidator = [
+  body('name').custom((val, { req }) => {
+    req.body.slug = slugify(val);
+    return true;
+  }),
+  check('email')
+    .notEmpty()
+    .withMessage('Email required')
+    .isEmail()
+    .withMessage('Invalid email address')
+    .custom(val =>
+      User.findOne({ email: val }).then(user => {
+        if (user) {
+          return Promise.reject(new Error('E-mail already in user'));
+        }
+      })
+    ),
+  check('password')
+    .notEmpty()
+    .withMessage('password is required')
+    .isLength({ min: 8 })
+    .withMessage('passwort must be at least 8 characters'),
+  check('phone')
+    .isMobilePhone(['ar-EG', 'ar-SA'])
+    .withMessage('Invalid phone number only accepted Egy and SA Phone numbers'),
+  validatorMiddleware
+];
+exports.loginValidator = [
+  check('email')
+    .notEmpty()
+    .withMessage('Email required'),
+  check('password')
+    .notEmpty()
+    .withMessage('password is required')
+    .isLength({ min: 8 })
+    .withMessage('passwort must be at least 8 characters'),
+  validatorMiddleware
+];
