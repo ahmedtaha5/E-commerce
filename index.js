704bf5806d587ff46df2bf4a0043a2d79af0c4e6
ahmedtaha5 <ahmedtaha1682@gmail.com>
2025-01-18 07:58:44 +0200

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const path = require('path');
-
 const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
@@ -11,7 +10,7 @@ const AppError = require('./utils/appError');
 const dbConnection = require('./Config/database');
 
 // Routes
-const mountRoutes = require('./routes/index');
+const mountRoutes = require('./routes/allRoutes');
 
 dotenv.config({ path: 'config.env' });
 
@@ -31,14 +30,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'uploads')));
 
 mountRoutes(app);
-// app.use('/api/v1/categories', categoryRoutes);
-// app.use('/api/v1/subcategories', subCategoryRoutes);
-// app.use('/api/v1/brands', brandRoutes);
-// app.use('/api/v1/products', productRoutes);
-// app.use('/api/v1/users', userRoutes);
-// app.use('/api/v1/auth', authRoutes);
-// app.use('/api/v1/reviews', reviewRoutes);
-// app.use('/api/v1/wishlist', wishlistRoutes);
 
 // Handle undefined routes
 app.all('*', (req, res, next) => {
@@ -53,12 +44,6 @@ if (process.env.NODE_ENV === 'development') {
 
 // Global error handling middleware for express errors
 app.use(globalError);
-// Start the server
-// Handle uncaught exceptions
-process.on('uncaughtException', err => {
-  console.error('Uncaught Exception Error:', `${err.name} | ${err.message}`);
-  process.exit(1); // Exit immediately for stability.
-});
 
 //Start the server
 const port = process.env.PORT || 3000;
@@ -66,12 +51,11 @@ const server = app.listen(port, () => {
   console.log(`API is running on port: ${port}`);
 });
 
-// Handle unhandled promise rejections
+// Handle rejection outside express
 process.on('unhandledRejection', err => {
-  console.error('Unhandled Rejection Error:', `${err.name} | ${err.message}`);
+  console.error(`UnhandledRejection Errors: ${err.name} | ${err.message}`);
   server.close(() => {
-    console.log('Server is closing down...');
-    // Close database connections or clean up resources
-    process.exit(1); // Exit with failure code.
+    console.error(`Shutting down....`);
+    process.exit(1);
   });
 });
